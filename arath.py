08c365b920b4a4ea5578b83e8e2e8bc17e8815f6
Alexander Gorelyshev <corvus-migratorius@proton.me>
2024-07-02 23:44:00 +0400

    Only consider plays younger than the previous timestamp

--- arath.py
+++ arath.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+from datetime import datetime as dt
 from pathlib import Path
 
 from ara.clients.offline import AraOfflineClient  # type: ignore
@@ -11,24 +12,58 @@ class Handler:
         self.client = AraOfflineClient()
         self.playbooks = []
         self.actions: list[dict] = []
+        
+        self.timestamp_path = ".arath.timestamp"
+        self.timestamp_now = dt.now().isoformat()
+        self.timestamp_prev = self.load_timestamp()
+
         self.report_template = """
 {status}: "{taskname}"
 {hostname} [{tagline}] "{playbook}"
 {filename}:{lineno}
         """.strip()
-        
+
+
+    def update_timestamp(self):
+        with open(self.timestamp_path, "w", encoding="utf-8") as file:
+            file.write(self.timestamp_now)
+
+
+    def load_timestamp(self) -> dt:
+        """
+        If timestamp file exists, return its contents, else epoch start.
+        """
+        try:
+            with open(self.timestamp_path, "r", encoding="utf-8") as file:
+                timestamp_prev = file.read()
+            return dt.fromisoformat(timestamp_prev)
+        except FileNotFoundError as error:
+            print(error)
+            return dt.fromisoformat("1970-01-01T00:00:00.0Z")
+
 
     def run(self) -> None:
         self.fetch_playbooks()
         self.fetch_actions()
         self.report()
 
+        # only if the run was successful
+        self.update_timestamp()
+
 
     def fetch_playbooks(self) -> None:
-        self.playbooks = self.client.get("/api/v1/playbooks", status=["completed", "failed"], order="ended")
+        self.playbooks = self.client.get(
+            "/api/v1/playbooks",
+            status=["completed", "failed"],
+            order="ended",
+            ended_after=self.timestamp_prev
+        )
 
 
     def fetch_actions(self) -> None:
+        print(f"current timestamp: {self.timestamp_now}")
+        print(f"previous timestamp: {self.timestamp_prev}")
+
         # For each result, print the task and host information
         for playbook in self.playbooks["results"]:
             results = self.client.get(f"/api/v1/results?playbook={playbook['id']}")["results"]
@@ -61,6 +96,7 @@ class Handler:
         """
         return [action for action in self.actions if action["status"] not in statuses]
 
+
     def mk_template(self, action: dict) -> str:
         modified = {
             "hostname": action["hostname_fact"] or action["hostname_inv"],
@@ -68,6 +104,7 @@ class Handler:
             **action
         }
         return self.report_template.format(**modified)
+
 
     def report(self) -> None:
         relevant = self.filter()
